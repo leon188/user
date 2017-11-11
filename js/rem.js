b9090a26e62a4a21a003e037f20c5792622daa5b
李晓菲 <13937257330@163.com>
2017-11-11 12:16:40 +0800

    s

---
+++ js/rem.js
@@ -0,0 +1,17 @@
+(function(win, doc) {
+		//浏览器缩放时
+		win.onresize = function() {
+			change();
+		};
+		change();
+
+		function change() {
+
+			//320  20px 
+			//414  20px
+			var oFs = doc.documentElement.clientWidth / (750 / 20);
+
+			//设置根目录字体大小
+			doc.documentElement.style.fontSize = oFs + 'px';
+		}
+	})(window, document);
